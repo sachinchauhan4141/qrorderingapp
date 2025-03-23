2cbfd49d3c8455d3688fbeed181018ecbe5cfa37
sachinchau4141 <chauhansahab85@gmail.com>
2025-03-23 18:20:36 +0530

    "fixing issues"

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -72,7 +72,7 @@ const Menu = () => {
           <select
             value={filters.category}
             onChange={(e) =>
-              setFilters({ ..?.filters, category: e.target.value })
+              setFilters({ ...filters, category: e.target.value })
             }
             className="p-2 bg-gray-200 rounded-lg text-sm text-gray-700"
           >
@@ -83,7 +83,7 @@ const Menu = () => {
           </select>
           <select
             value={filters.isVeg}
-            onChange={(e) => setFilters({ ..?.filters, isVeg: e.target.value })}
+            onChange={(e) => setFilters({ ...filters, isVeg: e.target.value })}
             className="p-2 bg-gray-200 rounded-lg text-sm text-gray-700"
           >
             <option value="">Veg/Non</option>
@@ -93,7 +93,7 @@ const Menu = () => {
           <select
             value={filters.inStock}
             onChange={(e) =>
-              setFilters({ ..?.filters, inStock: e.target.value })
+              setFilters({ ...filters, inStock: e.target.value })
             }
             className="p-2 bg-gray-200 rounded-lg text-sm text-gray-700"
           >
